2e040f8e34181f2a1e1626d2a90f928168875a36
iamageo <geovani.af4@gmail.com>
2021-04-18 19:50:35 -0300

    recyclerview example

---
+++ RecyclerView/app/src/main/java/com/iamageo/recyclerview/model/User.java
@@ -0,0 +1,17 @@
+package com.iamageo.recyclerview.model;
+
+public class User {
+    private String username;
+
+    public User(String name) {
+        this.username = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
