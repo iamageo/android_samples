0a8445cfcb9e0a0d70b20775d0aaeb4256f9ec45
Geovani Amaral <geovani.af4@gmail.com>
2021-06-01 22:12:22 -0300

    slider material design

---
+++ materialdesign/slider/app/src/main/java/com/iamageo/slider/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.iamageo.slider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        
+    }
+}
